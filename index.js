8ddb6893e37d2cc964fe218222d63209b4836b9f
Kaushang Surya <141481179+kaushang@users.noreply.github.com>
2023-08-10 19:25:53 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 function game() {
     // RANDOM NUMBER GENERATIONvar randomNumber;
     function randomNumberGenerater() {
-        randomNumber = Math.floor(Math.random() * 10 + 1);
+        randomNumber = Math.floor(Math.random() * 100 + 1);
         console.log(randomNumber);
     }
+    document.querySelector(".reload").style.visibility = "hidden";
     randomNumberGenerater();
-
+    // INITIALIZING SCORE
+    var a = 0;
     // NUMBER INSERTION AND CHECKING
-    var userChosenNumber
     document.querySelector("#button-addon2").addEventListener("click", function () {
-        userChosenNumber = document.querySelector(".form-control-file").value;
+        let userChosenNumber = document.querySelector(".form-control-file").value;
         Number.parseInt(userChosenNumber);
-
+        a++;
         if (userChosenNumber == randomNumber) {
             document.querySelector(".hint").innerHTML = "Right answer!";
-
+            document.querySelector("#chances").innerHTML = "Your score is " + Number(100-a);
+            document.querySelector(".reload").style.visibility = "visible";
+            document.querySelector(".reload").addEventListener("click", ()=> {
+                location.reload();
+            })            
         }
         else if (userChosenNumber > randomNumber) {
             document.querySelector(".hint").innerHTML = "The number is less than " + userChosenNumber;
@@ -23,13 +28,6 @@ function game() {
             document.querySelector(".hint").innerHTML = "The number is greater than " + userChosenNumber;
         }
     })
-
 }
 game();
-
-
-
-
-
-
 
